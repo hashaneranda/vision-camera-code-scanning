c25b412ae4adbb6ac43cb149c2ea52f8344365d5
hashaneranda <hashaneranda@gmail.com>
2023-09-18 02:22:56 +0530

    v0.0.3 added

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,24 +1,22 @@
 import * as React from 'react';
 
 import { StyleSheet, Text } from 'react-native';
-import { useCameraDevices } from 'react-native-vision-camera';
-import { Camera } from 'react-native-vision-camera';
-import { useScanBarcodes, BarcodeFormat } from 'vision-camera-code-scanner';
+import { Camera, useCameraDevices } from 'react-native-vision-camera';
+import { BarcodeFormat, useScanBarcodes } from 'vision-camera-code-scanner';
 
 export default function App() {
   const [hasPermission, setHasPermission] = React.useState(false);
+  const [frameProcessor, barcodes] = useScanBarcodes([
+    BarcodeFormat.ALL_FORMATS,
+    BarcodeFormat.QR_CODE,
+  ]);
   const devices = useCameraDevices();
   const device = devices.back;
-
-  const [frameProcessor, barcodes] = useScanBarcodes(
-    [BarcodeFormat.ALL_FORMATS],
-    { checkInverted: true }
-  );
 
   React.useEffect(() => {
     (async () => {
       const status = await Camera.requestCameraPermission();
-      setHasPermission(status === 'authorized');
+      setHasPermission(status === 'granted');
     })();
   }, []);
 
@@ -35,7 +33,6 @@ export default function App() {
           device={device}
           isActive={true}
           frameProcessor={frameProcessor}
-          frameProcessorFps={5}
         />
         {barcodes.map((barcode, idx) => (
           <Text key={idx} style={styles.barcodeTextURL}>
